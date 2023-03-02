de1deea8841b887397ed80e4d9cc354a71077bab
Yaya LY <yaya.ly@uadb.edu.sn>
2023-03-02 10:07:25 +0000

    form design

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ function openMenu() {
 function closeMenu() {
     sidemenu.style.right = "-200px";
 }
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxKGN9ctJO3AVvg3QgBcEmAsZlZkfLp5FdBOVoN54u-UQw0lkLrAk9S9dW7vmMMxR30cw/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+})
